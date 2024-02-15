def3a50cb3eae3b9f858569730c5101db91506a2
yunasim <ynsim@solidit.co.kr>
2024-02-15 23:14:42 +0900

    #😎update: Property Dependency 구현하여, 컨트롤과 연결

--- WPFDependencyProperty/CalculateControl.xaml.cs
+++ WPFDependencyProperty/CalculateControl.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +20,71 @@ namespace WPFDependencyProperty
     /// <summary>
     /// CalculateControl.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class CalculateControl : UserControl
+    public partial class CalculateControl : UserControl, INotifyPropertyChanged
     {
         public CalculateControl()
         {
             InitializeComponent();
+        }
+
+        public static readonly DependencyProperty Value1Property =
+            DependencyProperty.Register("Value1", typeof(decimal), typeof(CalculateControl), new PropertyMetadata(0m, OnValueChanged));
+
+        public static readonly DependencyProperty Value2Property =
+            DependencyProperty.Register("Value2", typeof(decimal), typeof(CalculateControl), new PropertyMetadata(0m, OnValueChanged));
+
+        public static readonly DependencyProperty OperatorProperty =
+            DependencyProperty.Register("Operator", typeof(string), typeof(CalculateControl), new PropertyMetadata("+", OnValueChanged));
+
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            CalculateControl calculateControl = (CalculateControl)d;
+            calculateControl.OnPropertyChanged(nameof(Result));
+        }
+
+        public string Operator
+        {
+            get { return (string)GetValue(OperatorProperty); }
+            set { SetValue(OperatorProperty, value); }
+        }
+
+        public decimal Value1
+        {
+            get { return (decimal)GetValue(Value1Property); }
+            set { SetValue(Value1Property, value); }
+        }
+
+
+        public decimal Value2
+        {
+            get { return (decimal)GetValue(Value2Property); }
+            set { SetValue(Value2Property, value); }
+        }
+
+        public decimal Result => Operator switch
+        {
+            "+" => Value1 + Value2,
+            "-" => Value1 - Value2,
+            "*" => Value1 * Value2,
+            "/" => Value2 == 0
+                ? 0
+                : Math.Round(Value1 / Value2, 2),
+            _ => 0m
+        };
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
         }
     }
 }
